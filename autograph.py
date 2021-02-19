d1e1a9273c957764639e763cbf58a0d024e83f89
Mardukas <svahnv@gmail.com>
2021-02-19 13:39:24 +0100

    Add files via upload

--- autograph.py
+++ autograph.py
@@ -27,43 +27,59 @@ from scipy.optimize import curve_fit
 ## PREAMBLE ##
 ##############
 
-# Figure style. Choose between; standard, latex, latex-math.
-use_style = 'standard'
-
 # Specify title and axis labels.
 plot_title = 'Title'
 x_axis_label = 'x'
 y_axis_label = 'y'
-title_fontsize = 18
-axis_fontsize = 16
-axis_ticksize = 14
-scatter_point_type ='.' # See matplotlib documentation for variants, e.g. 'o', 's' etc.
-legend_position = '' # Set legend position. If empty 'best' is chosen.
-
-# Scales data according to given factors
-x_axis_factor = 1
-y_axis_factor = 1
 
 # Set type of curve fitting and alpha on scatter points
-curve_fit_type = 'none' # Choose from: none, lin-reg, mono-exp.
-scatter_alpha_amount = 0.8 # Sets transparency of scatter points.
+curve_fit_type = 'mono-exp' # Choose from: none, lin-reg, mono-exp.
+scatter_alpha_amount = 0.3 # Sets transparency of scatter points.
 
-# Input output file specifics such as file- type, name, resoltion.
+# Output file specifics such as file- type, name, location.
 show_plot = True
 savefile = False
 save_name = ''
 save_location = ''
-resolution_dpi = 300
-figure_format = 'eps'
 
 # Input file location and filnames. Files takes a list of filenames with number of rows to skip in each file.
 # The skip functionality is used to ignore non-data rows of output files from instruments an such.
 # Ex. files = [ ['filename1', skiprows], ['filename2', skiprows] ]
 file_location = 'Data/'
 files = [
-	['data1', 0],
-	['data2', 0]
+	['exp_data', 0],
+	['exp_data2', 0]
 ]
+
+
+#######################
+## ADVANCED SETTINGS ##
+#######################
+
+# Figure style. Choose between; standard, latex, latex-math.
+use_style = 'latex'
+
+# Input output file specifics such as file- type, name, resoltion.
+resolution_dpi = 300
+figure_format = 'eps'
+
+
+
+
+# Plot specifics, fontsize, ticksize, scatter poit--type, legend position other than 'best'.
+set_fontsize = False
+title_fontsize = 18
+axis_fontsize = 16
+axis_ticksize = 14
+scatter_point_type ='.' # See matplotlib documentation for variants, e.g. 'o', 's' etc.
+legend_position = '' # Set legend position. If empty 'best' is chosen.
+
+
+# Axis scaling factor. Affects all input data equally.
+# Scales data according to given factors
+x_axis_factor = 1
+y_axis_factor = 1
+
 
 ###########################################################################################################################################
 ###########################################################################################################################################
@@ -75,6 +91,7 @@ files = [
 # clearly associated with a specific file.
 files_df = pd.DataFrame(files, columns = ['Filename', 'Skiprows'])
 files_index = files_df.index
+print('Input:')
 print(files_df)
 print('Make sure the correct number of lines are skipped in each data file.')
 
@@ -121,7 +138,7 @@ def exponential(x, a, b, c):
 	"""
 	Defines an exponential functions to be fit to data points.
 	"""
-	return a * np.exp(-b * x) + c
+	return a * np.exp(b * x) + c
 
 def linear_fit(x,y):
 	"""
@@ -133,38 +150,42 @@ def mono_exp_fit(x,y):
 	"""
 	Applies a mono-exponential fit to x,y. The fitting parameters are guessed automatically.
 
-	The starting index is chosen as the absolute maximum value of the y-axis. If the integral
-	is positive this value is max(y) whereas a negative integral means it is min(y). These are
-	respectively, the intercepts as well.
-
-	The slope of a mono-exponential is given by slope = ln(2)/x_half, where x_half denotes
+	The slope of a mono-exponential is given by slope = -ln(2)/x_half, where x_half denotes
 	the position along the x-axis where y is at half maximum.
 
 	To estimate the error, the mean value is taken over the first or last 2% elements, rounded 
 	to the nearest integer, of the number of points in y. The first values are chosen if the
 	exponential is of growth type, otherwise the last values or chosen.
+	
+
+	The starting index is chosen as the absolute maximum value of the y-axis. If the value of
+	y = y/2 is positive this value is max(y) whereas a negative value means it is min(y). 
+	These are, respectively, the intercepts as well.
 	"""
 
-	# Determination of starting index and guessing of intercept.
-	if np.trapz(y, x=x) > 0:
-		start_index = np.argmax(y)
-		intercept_guess = max(y)
-	else:
-		start_index = np.argmin(y)
-		intercept_guess = min(y)
-
+	
 	# Slope estimation. Index of mid y-value is found. 
 	y_half = find_nearest(y, ( max(y) + min(y) )/2 )
 	y_half_id = np.where( y == y_half )
 	x_at_half_y = x[ y_half_id ]
-	slope_guess = float( np.log(2)/x_at_half_y )
-
-	# Error estimation.
+	slope_guess = -float( np.log(2)/x_at_half_y )
+	
+	# Number of values error_guess is based on.
 	error_lower_bound_index = math.ceil(len(y)*0.02)
-	if slope_guess < 0:
-		error_guess = np.mean( y[-error_lower_bound_index:] )
-	else:
+
+	if slope_guess > 0:
 		error_guess = np.mean( y[:error_lower_bound_index] )
+		x_zero = find_nearest(x, 0)
+		x_zero_index = np.where( x == x_zero )
+		intercept_guess = float( y[ x_zero_index ])
+		result = [ intercept_guess, slope_guess, error_guess ]
+		print(result)
+	else:
+		error_guess = np.mean( y[-error_lower_bound_index:] )
+		if y_half > 0:
+			intercept_guess = max(y)
+		else:
+			intercept_guess = min(y)
 
 	# Curve fitting.
 	popt, pcov = curve_fit(
@@ -183,6 +204,8 @@ def mono_exp_fit(x,y):
 ##########
 ## PLOT ##
 ##########
+
+output = []
 
 # For loop that iterates and plots over all specfied files.
 for i in files_index:
@@ -227,6 +250,11 @@ for i in files_index:
 		# Plots linear regression
 		if curve_fit_type == 'lin-reg':
 			res = linear_fit(x_values, y_values)
+
+			# Prepare result ouput.
+			result = [ filename, res.intercept, res.slope, res.rvalue**2 ]
+			output.append(result)
+
 			plt.plot(
 				x, 
 				res.intercept + res.slope*x, 
@@ -239,18 +267,39 @@ for i in files_index:
 			else:
 				start_index = np.argmin(y_values)
 			popt, pcov = mono_exp_fit(x_values, y_values)
+
+			# Prepare result ouput.
+			result = [ filename, popt[0], popt[1], popt[2] ]
+			output.append(result)			
+
 			plt.plot(
 				x_values[start_index:, ], 
 				exponential(x_values[start_index:, ], 
 				*popt), 
 				label='$y= %6.5E e^{ %6.5E t} %+6.5E $' % tuple(popt) 
 				) 
+	
+# Results output in a DataFrame environment.
 
+if curve_fit_type == 'lin-reg':
+	output_df = pd.DataFrame(output, columns = [ 'Filename', 'Intercept', 'Slope', 'R-squared'] )
+	print('Output:')
+	print(output_df)
+elif curve_fit_type == 'mono-exp':		
+	output_df = pd.DataFrame(output, columns = [ 'Filename', 'Intercept', 'Slope', 'Error'] )
+	print('Output:')
+	print(output_df)
 
-plt.title(plot_title, fontsize=title_fontsize)
-plt.xlabel(x_axis_label, fontsize=axis_fontsize)
-plt.ylabel(y_axis_label, fontsize=axis_fontsize)
-plt.tick_params(labelsize=axis_ticksize)
+	
+if set_fontsize:
+	plt.title(plot_title, fontsize=title_fontsize)
+	plt.xlabel(x_axis_label, fontsize=axis_fontsize)
+	plt.ylabel(y_axis_label, fontsize=axis_fontsize)
+	plt.tick_params(labelsize=axis_ticksize)
+else:
+	plt.title(plot_title)
+	plt.xlabel(x_axis_label)
+	plt.ylabel(y_axis_label)
 
 if legend_position == '':
 	plt.legend(loc='best')
